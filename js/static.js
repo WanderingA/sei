a9ea3e98b7f276f242600a09e32d0b426e404d17
wang <1940004351@qq.com>
2023-06-24 15:36:42 +0800

    6-24-224

--- js/static.js
+++ js/static.js
@@ -5,9 +5,9 @@ $(function(){
 
     char1();
     char2();
+    // char3();
+    // char4();
     char3();
-    char4();
-    char5();
 })
 
 //统计分析图
@@ -27,13 +27,13 @@ function char1() {
                 color : '#ffffff',
 
             },
-            data:['客运车','危险品车','网约车','学生校车']
+            data:['人','汽车','履带车','飞行器']
         },
 
         calculable : false,
         series : [
             {
-                name:'车类型',
+                name:'类型',
                 type:'pie',
                 radius : ['40%', '70%'],
                 itemStyle : {
@@ -57,10 +57,10 @@ function char1() {
                     }
                 },
                 data:[
-                    {value:335, name:'客运车'},
-                    {value:310, name:'危险品车'},
-                    {value:234, name:'网约车'},
-                    {value:135, name:'学生校车'}
+                    {value:231, name:'人'},
+                    {value:132, name:'汽车'},
+                    {value:13, name:'履带车'},
+                    {value:3, name:'飞行器'}
 
                 ]
             }
@@ -166,147 +166,147 @@ function char2() {
     window.addEventListener('resize', function () {myChart.resize();})
 
 }
+// function char3() {
+
+//     var myChart = echarts.init($("#char3")[0]);
+
+//     option = {
+//         legend: {
+//             data:['车辆行驶数量'],
+//             textStyle : {
+//                 color : '#ffffff',
+
+//             }
+//         },
+//         grid: {show:'true',borderWidth:'0'},
+
+//         calculable : false,
+//         tooltip : {
+//             trigger: 'axis',
+//             formatter: "Temperature : <br/>{b}km : {c}°C"
+//         },
+//         xAxis : [
+//             {
+//                 type : 'value',
+//                 axisLabel : {
+//                     formatter: '{value}',
+//                     textStyle: {
+//                         color: '#fff'
+//                     }
+//                 },
+
+//                 splitLine:{
+//                     lineStyle:{
+//                         width:0,
+//                         type:'solid'
+//                     }
+//                 }
+//             }
+//         ],
+//         yAxis : [
+//             {
+//                 type : 'category',
+//                 axisLine : {onZero: false},
+//                 axisLabel : {
+//                     formatter: '{value} km',
+//                     textStyle: {
+//                         color: '#fff'
+//                     }
+//                 },
+//                 splitLine:{
+//                     lineStyle:{
+//                         width:0,
+//                         type:'solid'
+//                     }
+//                 },
+//                 boundaryGap : false,
+//                 data : ['0', '10', '20', '30', '40', '50', '60', '70', '80']
+//             }
+//         ],
+//         series : [
+//             {
+//                 name:'车辆行驶数量',
+//                 type:'line',
+//                 smooth:true,
+//                 itemStyle: {
+//                     normal: {
+//                         lineStyle: {
+//                             shadowColor : 'rgba(0,0,0,0.4)'
+//                         }
+//                     }
+//                 },
+//                 data:[15, 0, 20, 45, 22.1, 25, 70, 55, 76]
+//             }
+//         ]
+//     };
+
+//     myChart.setOption(option);
+//     window.addEventListener('resize', function () {myChart.resize();})
+
+// }
+// function char4() {
+
+//     var myChart = echarts.init($("#char4")[0]);
+
+//     option = {
+//         grid: {show:'true',borderWidth:'0'},
+//         tooltip : {
+//             trigger: 'axis',
+//             axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+//                 type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+//             },
+
+//             formatter: function (params) {
+//                 var tar = params[0];
+//                 return tar.name + '<br/>' + tar.seriesName + ' : ' + tar.value;
+//             }
+//         },
+
+//         xAxis : [
+//             {
+//                 type : 'category',
+//                 splitLine: {show:false},
+//                 data : ['客运车','危险品车','网约车','学生校车'],
+//                 axisLabel: {
+//                     show: true,
+//                     textStyle: {
+//                         color: '#fff'
+//                     }
+//                 }
+
+//             }
+//         ],
+//         yAxis : [
+//             {
+//                 type : 'value',
+//                 splitLine: {show:false},
+//                 axisLabel: {
+//                     show: true,
+//                     textStyle: {
+//                         color: '#fff'
+//                     }
+//                 }
+//             }
+//         ],
+//         series : [
+
+//             {
+//                 name:'报警数量',
+//                 type:'bar',
+//                 stack: '总量',
+//                 itemStyle : { normal: {label : {show: true, position: 'inside'}}},
+//                 data:[2900, 1200, 300, 200, 900, 300]
+//             }
+//         ]
+//     };
+
+//     myChart.setOption(option);
+//     window.addEventListener('resize', function () {myChart.resize();})
+
+// }
 function char3() {
 
     var myChart = echarts.init($("#char3")[0]);
-
-    option = {
-        legend: {
-            data:['车辆行驶数量'],
-            textStyle : {
-                color : '#ffffff',
-
-            }
-        },
-        grid: {show:'true',borderWidth:'0'},
-
-        calculable : false,
-        tooltip : {
-            trigger: 'axis',
-            formatter: "Temperature : <br/>{b}km : {c}°C"
-        },
-        xAxis : [
-            {
-                type : 'value',
-                axisLabel : {
-                    formatter: '{value}',
-                    textStyle: {
-                        color: '#fff'
-                    }
-                },
-
-                splitLine:{
-                    lineStyle:{
-                        width:0,
-                        type:'solid'
-                    }
-                }
-            }
-        ],
-        yAxis : [
-            {
-                type : 'category',
-                axisLine : {onZero: false},
-                axisLabel : {
-                    formatter: '{value} km',
-                    textStyle: {
-                        color: '#fff'
-                    }
-                },
-                splitLine:{
-                    lineStyle:{
-                        width:0,
-                        type:'solid'
-                    }
-                },
-                boundaryGap : false,
-                data : ['0', '10', '20', '30', '40', '50', '60', '70', '80']
-            }
-        ],
-        series : [
-            {
-                name:'车辆行驶数量',
-                type:'line',
-                smooth:true,
-                itemStyle: {
-                    normal: {
-                        lineStyle: {
-                            shadowColor : 'rgba(0,0,0,0.4)'
-                        }
-                    }
-                },
-                data:[15, 0, 20, 45, 22.1, 25, 70, 55, 76]
-            }
-        ]
-    };
-
-    myChart.setOption(option);
-    window.addEventListener('resize', function () {myChart.resize();})
-
-}
-function char4() {
-
-    var myChart = echarts.init($("#char4")[0]);
-
-    option = {
-        grid: {show:'true',borderWidth:'0'},
-        tooltip : {
-            trigger: 'axis',
-            axisPointer : {            // 坐标轴指示器，坐标轴触发有效
-                type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-            },
-
-            formatter: function (params) {
-                var tar = params[0];
-                return tar.name + '<br/>' + tar.seriesName + ' : ' + tar.value;
-            }
-        },
-
-        xAxis : [
-            {
-                type : 'category',
-                splitLine: {show:false},
-                data : ['客运车','危险品车','网约车','学生校车'],
-                axisLabel: {
-                    show: true,
-                    textStyle: {
-                        color: '#fff'
-                    }
-                }
-
-            }
-        ],
-        yAxis : [
-            {
-                type : 'value',
-                splitLine: {show:false},
-                axisLabel: {
-                    show: true,
-                    textStyle: {
-                        color: '#fff'
-                    }
-                }
-            }
-        ],
-        series : [
-
-            {
-                name:'报警数量',
-                type:'bar',
-                stack: '总量',
-                itemStyle : { normal: {label : {show: true, position: 'inside'}}},
-                data:[2900, 1200, 300, 200, 900, 300]
-            }
-        ]
-    };
-
-    myChart.setOption(option);
-    window.addEventListener('resize', function () {myChart.resize();})
-
-}
-function char5() {
-
-    var myChart = echarts.init($("#char5")[0]);
 
     option = {
         grid: {show:'true',borderWidth:'0'},
